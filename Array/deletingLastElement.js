ce07ea3717b2d1d731aef80eccd57ddf7078e03c
Sarthak231203 <>
2024-05-05 22:33:11 +0530

    more simplicity is been added in deletion of last element code

--- Array/deletingLastElement.js
+++ Array/deletingLastElement.js
@@ -1,25 +1,38 @@
 
-// Deleting the last element from the array:-
+// 1.Deleting the last element from the array:-
 // Array will get modified hence we can't create any new array.
 
-const array = [1,2,3,4,5];
+const a = [1,2,3,4,5,6,7];
 
-function deleting(n , index)
-{
-    if(index < 0 || index > n.length){
-        return -1;
-    }
-    else{
-        for(let i = index ; i < n.length - 1; i++)
-            {
-                n[i] = n[i+1];
-            }
-            n.pop();
-            return n;
+let index = 3;
+
+for(let i = index; i < a.length - 1; i++)
+    {
+        a[i] = a[i+1];
     }
 
+    a.pop();
+
+console.log(`Array after deletion of data :- ${a}`);
+
+//2.Write Second program using splice for deleting and inserting the element on index 3;
+//Add the value 9 on index 3.
+
+
+
+// Using splice function:-
+
+function usingSplice(n , index){
+    // Remove 1 elements starting from index value and add value 9
+    n.splice(index , 1, 9)
+    return n;
 }
 
-const afterDeletion = deleting(array, 5);
+const value = usingSplice(a , 3);
 
-console.log(`Array after deletion of data :- ${afterDeletion}`);
+console.log(`After using splice now array is ${value}`);
+
+
+// splice() is a built-in method in JavaScript that allows you to modify the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+
+// array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
